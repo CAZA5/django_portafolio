d25c1bd595aafeb569cd04609fa87e01851c250b
unknown <gdcaza33@gmail.com>
2024-04-18 15:30:03 -0600

    900

--- build.sh
+++ build.sh
@@ -6,11 +6,12 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
+pip freeze > requirements.txt
 python manage.py collectstatic --no-input
 pip install -r requirements.txt
 # Apply any outstanding database migrations
 #install gunicorn
-pip install gunicorn
+pipenv install gunicorn
 gunicorn web_personal.wsgi
 python manage.py migrate
 chmod a+x build.sh
